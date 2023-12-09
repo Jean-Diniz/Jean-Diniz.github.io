8f08804a3c3f920f344370084756ac03cd9a0138
Jean Santos Diniz <jeansantosdiniz2001@gmail.com>
2023-12-09 18:39:59 -0300

    Add files via upload

---
+++ ignews/src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import { AppProps } from "next/app";
+import { SessionProvider as NextAuthProvider } from "next-auth/react";
+
+import "../../styles/globals.scss";
+import { Header } from "../components/Header";
+
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  return (
+    <NextAuthProvider session={pageProps.session}>
+      <Header />
+      <Component {...pageProps} />
+    </NextAuthProvider>
+  );
+}
+
+export default MyApp;
